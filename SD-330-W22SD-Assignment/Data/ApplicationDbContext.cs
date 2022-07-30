6f23c01b5ca0374cca73ce0aa007b671970a72e5
Jaesung Hong <jezsung@gmail.com>
2022-07-30 18:31:33 -0500

    Initial commit

---
+++ SD-330-W22SD-Assignment/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SD_330_W22SD_Assignment.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
