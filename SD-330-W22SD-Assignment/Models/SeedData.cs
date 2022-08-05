949caee42480c53a7ee1f82002022ffa81e64a19
Jaesung Hong <jezsung@gmail.com>
2022-08-05 11:31:40 -0500

    add roles

---
+++ SD-330-W22SD-Assignment/Models/SeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SD_330_W22SD_Assignment.Data;
+
+namespace SD_330_W22SD_Assignment.Models
+{
+    public static class SeedData
+    {
+        public async static Task Initialize(IServiceProvider serviceProvider)
+        {
+            var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            List<string> roles = new List<string>
+            {
+                "Administrator", "User"
+            };
+
+            foreach (string role in roles)
+            {
+                var identityRole = new IdentityRole(role);
+
+                if (!context.Roles.Contains(identityRole))
+                {
+                    await roleManager.CreateAsync(identityRole);
+                }
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
