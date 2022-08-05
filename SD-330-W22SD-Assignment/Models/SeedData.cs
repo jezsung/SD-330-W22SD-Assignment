56131da7a7dfb491fbf7744f8220dd7db14627da
Jaesung Hong <jezsung@gmail.com>
2022-08-05 12:06:45 -0500

    add seed users

--- SD-330-W22SD-Assignment/Models/SeedData.cs
+++ SD-330-W22SD-Assignment/Models/SeedData.cs
@@ -27,6 +27,63 @@ namespace SD_330_W22SD_Assignment.Models
                 }
             }
 
+            if (context.Users.FirstOrDefault(u => u.Email == "admin@test.com") == null)
+            {
+                var admin = new ApplicationUser
+                {
+                    Email = "admin@test.com",
+                    NormalizedEmail = "ADMIN@TEST.COM",
+                    UserName = "admin@test.com",
+                    NormalizedUserName = "ADMIN@TEST.COM",
+                    EmailConfirmed = true,
+                };
+                var password = new PasswordHasher<ApplicationUser>();
+                var hashed = password.HashPassword(admin, "Password1@");
+                admin.PasswordHash = hashed;
+
+
+                await userManager.CreateAsync(admin);
+                await userManager.AddToRoleAsync(admin, "Administrator");
+            }
+
+            if (context.Users.FirstOrDefault(u => u.Email == "user1@test.com") == null)
+            {
+                var user1 = new ApplicationUser
+                {
+                    Email = "user1@test.com",
+                    NormalizedEmail = "USER1@TEST.COM",
+                    UserName = "user1@test.com",
+                    NormalizedUserName = "USER1@TEST.COM",
+                    EmailConfirmed = true,
+                };
+                var password = new PasswordHasher<ApplicationUser>();
+                var hashed = password.HashPassword(user1, "Password1@");
+                user1.PasswordHash = hashed;
+
+
+                await userManager.CreateAsync(user1);
+                await userManager.AddToRoleAsync(user1, "User");
+            }
+
+            if (context.Users.FirstOrDefault(u => u.Email == "user2@test.com") == null)
+            {
+                var user1 = new ApplicationUser
+                {
+                    Email = "user2@test.com",
+                    NormalizedEmail = "USER2@TEST.COM",
+                    UserName = "user2@test.com",
+                    NormalizedUserName = "USER2@TEST.COM",
+                    EmailConfirmed = true,
+                };
+                var password = new PasswordHasher<ApplicationUser>();
+                var hashed = password.HashPassword(user1, "Password1@");
+                user1.PasswordHash = hashed;
+
+
+                await userManager.CreateAsync(user1);
+                await userManager.AddToRoleAsync(user1, "User");
+            }
+
             await context.SaveChangesAsync();
         }
     }
