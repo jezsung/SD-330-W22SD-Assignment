f128d70b361b773938449289085d98e3d1575d83
Jaesung Hong <jezsung@gmail.com>
2022-08-05 11:19:34 -0500

    add reputation to users

--- SD-330-W22SD-Assignment/Models/ApplicationUser.cs
+++ SD-330-W22SD-Assignment/Models/ApplicationUser.cs
@@ -4,6 +4,8 @@ namespace SD_330_W22SD_Assignment.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public int Reputation { get; set; }
+
         public virtual ICollection<Question> Questions { get; set; } = new List<Question>();
         public virtual ICollection<Answer> Answers { get; set; } = new List<Answer>();
         public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
